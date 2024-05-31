3e8f713f952390f17d80e8ad2d5c4c4a241d2c6d
CallMeJaeger <32680872+DaniKurosaki@users.noreply.github.com>
2024-05-31 05:41:16 +0200

    feature[4]: Add pagination, sorting and loading state

--- projects/front/src/app/modules/pages/page-home/page-home.component.ts
+++ projects/front/src/app/modules/pages/page-home/page-home.component.ts
@@ -19,105 +19,200 @@ import { HeroService } from "../../core/services/hero.service";
 import { HeroKeysLocalized } from "../../shared/enums/hero.enum";
 import { RouteEnum } from "../../core/constants/routes";
 import { IHero } from "../../../../../../back/src/interfaces/hero.interface";
-import { Genders } from "../../../../../../back/src/enums/common.enum";
 import { GenderLocalized } from "../../shared/enums/common.enum";
+import { Subject, debounceTime, distinctUntilChanged } from "rxjs";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
+/**
+ * Extra columns for the table that are not part of the data model
+ */
 type ExtraColumns = "edit" | "delete";
 
+/**
+ * Localized texts for the extra columns
+ 
+ */
 const ExtraColumnsLocalized: Record<ExtraColumns, string> = {
 	["edit"]: "Edit",
 	["delete"]: "Delete",
 };
 
+/**
+ * Localized text keys for dialog's messages
+ */
 type DialogTexts = keyof DialogData;
 
+/**
+ * Localized texts for the dialog messages
+ */
 const DialogTextsLocalized: Record<DialogTexts, string> = {
 	["title"]: "Are you sure you want to delete ",
 	["cancelText"]: "Cancel",
 	["confirmText"]: "Confirm",
 };
 
-type Messages = "NO_DATA_MATCHING";
+/**
+ * Localized text keys for this component messages
+ */
+type Messages = "NO_DATA_MATCHING" | "NO_DATA" | "ERROR";
 
+/**
+ * Localized texts for this component messages
+ */
 const MessagesLocalized: Record<Messages, string> = {
 	["NO_DATA_MATCHING"]: "No data matching the filter ",
+	["NO_DATA"]: "No data to display",
+	["ERROR"]: "An error occurred while processing the request",
 };
 
+/**
+ * This page serve as the home page for the application, it displays a table with all the heroes and allows the user to interact with them
+ */
 @Component({
 	selector: "page-home",
 	standalone: true,
-	imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatPaginator, MatSortModule, MatTableModule],
+	imports: [
+		CommonModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		MatPaginator,
+		MatProgressSpinnerModule,
+		MatSortModule,
+		MatTableModule,
+	],
 	templateUrl: "./page-home.component.html",
 	styleUrl: "./page-home.component.scss",
 })
 export class PageHomeComponent extends CommonComponent implements AfterViewInit {
+	/**
+	 * Injected dependency for Router
+	 */
 	protected readonly router: Router = inject(Router);
+
+	/**
+	 * Injected dependency for Material Dialog
+	 */
 	protected readonly dialog: MatDialog = inject(MatDialog);
+
+	/**
+	 * Injected dependency for Hero Service
+	 */
 	protected readonly heroService: HeroService = inject(HeroService);
+
+	/**
+	 * Table's paginator
+	 */
 	@ViewChild(MatPaginator) paginator!: MatPaginator;
-	// TODO - Fix this, not working for some strange reason
+
+	/**
+	 * Table's sort
+	 */
 	@ViewChild(MatSort) sort!: MatSort;
 
+	/**
+	 * Table's data source
+	 */
 	public dataSource: MatTableDataSource<IHero> = new MatTableDataSource<IHero>();
 
+	/**
+	 * Filter queue to debounce the filter input
+	 */
+	public filterQueue$: Subject<string> = new Subject<string>();
+
+	/**
+	 * Columns to be displayed in the table
+	 */
 	protected readonly displayedColumns: (keyof IHero | ExtraColumns)[] = ["first_name", "last_name", "hero_name", "gender", "edit", "delete"];
-	protected readonly GenderLocalized: Record<Genders, string> = GenderLocalized;
+
+	/**
+	 * Localized texts for the Genders
+	 */
+	protected readonly GenderLocalized: Record<string, string> = GenderLocalized;
+
+	/**
+	 * Localized texts for the Hero keys
+	 */
 	protected readonly localizedColumns: Record<keyof IHero, string> = HeroKeysLocalized;
+
+	/**
+	 * Localized texts for the extra columns
+	 */
 	protected readonly localizedExtraColumns: Record<ExtraColumns, string> = ExtraColumnsLocalized;
+
+	/**
+	 * Localized texts for the dialog messages
+	 */
 	protected readonly localizedMessages: Record<Messages, string> = MessagesLocalized;
 
+	/**
+	 * Override of the load method to fetch the data from the Hero Service
+	 */
 	override load(): void {
 		this.subscriptions.push(
-			this.heroService.getAll().subscribe({
+			this.heroService.getById("this.route.snapshot.paramMap.get()").subscribe({
 				next: (data) => {
-					this.dataSource = new MatTableDataSource<IHero>(data);
+					// if (!data.length) return;
+
+					// this.dataSource.data = data;
 				},
 				error: (error) => {
 					if (isDevMode()) console.error(error);
 				},
+			}),
+
+			this.filterQueue$.pipe(debounceTime(300), distinctUntilChanged()).subscribe((filterValue) => {
+				this.dataSource.filter = filterValue.trim().toLowerCase();
+
+				if (!this.dataSource.paginator) return;
+				this.dataSource.paginator.firstPage();
+				this.dataSource._updatePaginator(this.dataSource.filteredData.length);
 			})
 		);
 	}
 
+	/**
+	 * After view init lifecycle hook
+	 */
 	ngAfterViewInit() {
 		this.dataSource.paginator = this.paginator;
 		this.dataSource.sort = this.sort;
-		this.dataSource.sortingDataAccessor = (item: IHero, property: string) => {
-console.log("item", item);
-console.log("property", property);
-
-			switch (property) {
-				case "first_name":
-					return item.first_name.toLowerCase();
-				case "last_name":
-					return item.last_name.toLowerCase();
-				case "hero_name":
-					return item.hero_name.toLowerCase();
-				case "gender":
-					return item.gender;
-				default:
-					return item[property as keyof IHero] + "";
-			}
-		};
 	}
 
-	applyFilter(event: Event) {
+	/**
+	 * Sends the filter value to the filter queue
+	 * @param event
+	 */
+	protected applyFilter(event: Event) {
 		const filterValue = (event.target as HTMLInputElement).value;
-		this.dataSource.filter = filterValue.trim().toLowerCase();
 
-		if (!this.dataSource.paginator) return;
-		this.dataSource.paginator.firstPage();
+		this.filterQueue$.next(filterValue);
 	}
 
-	public interact(action: ExtraColumns, hero: IHero): void {
+	/**
+	 * Interact with the hero based on the action
+	 * @param action Action to be performed
+	 * @param hero Hero to interact with
+	 */
+	protected interact(action: ExtraColumns, hero: IHero): void {
 		this[action](hero);
 	}
 
-	public edit(hero: IHero): void {
+	/**
+	 * Redirects to the edit page
+	 * @param hero Hero to be edited, ID is passed as a parameter to the route
+	 */
+	private edit(hero: IHero): void {
 		this.router.navigate([RouteEnum.EDIT, hero.id]);
 	}
 
-	public delete(hero: IHero): void {
+	/**
+	 * Opens a dialog to confirm the deletion of the hero.
+	 * If the action is confirmed, calls the delete method from the Hero Service and updates the table.
+	 * @param hero Hero to be deleted
+	 */
+	private delete(hero: IHero): void {
 		const dialogRef = this.dialog.open(ModalConfirmComponent, {
 			data: {
 				title: `${DialogTextsLocalized["title"]} ${hero.hero_name}?`,
@@ -131,10 +226,16 @@ console.log("property", property);
 		dialogRef.afterClosed().subscribe((result) => {
 			if (!result) return;
 
-			this.heroService.deleteById(hero.id).subscribe((data) => {
-				const index = this.dataSource.data.findIndex((item) => item.id === hero.id);
-				this.dataSource.data.splice(index, 1);
-				this.dataSource._updateChangeSubscription();
+			this.heroService.deleteById(hero.id).subscribe({
+				next: (_) => {
+					const index = this.dataSource.data.findIndex((item) => item.id === hero.id);
+					this.dataSource.data.splice(index, 1);
+					this.dataSource._updateChangeSubscription();
+				},
+				error: (error) => {
+					if (isDevMode()) console.error(error);
+
+				},
 			});
 		});
 	}
