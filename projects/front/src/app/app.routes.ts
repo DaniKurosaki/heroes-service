30552fc3007c598e050d141b7c3471f68a13929d
CallMeJaeger <32680872+DaniKurosaki@users.noreply.github.com>
2024-06-01 06:29:37 +0200

    refactor[2]: Accomodate project structure for better setup (#10)

--- projects/front/src/app/app.routes.ts
+++ projects/front/src/app/app.routes.ts
@@ -1,26 +1,26 @@
 import { Routes } from "@angular/router";
-import { RouteEnum } from "./modules/core/constants/routes";
-import { PageHomeComponent } from "./modules/pages/page-home/page-home.component";
-import { PageCreateComponent } from "./modules/pages/page-create/page-create.component";
-import { PageEditComponent } from "./modules/pages/page-edit/page-edit.component";
+import { HeroSubRouteEnum } from "./modules/core/constants/routes";
+import { HeroListComponent } from "./modules/features/heroes/pages/hero-list/hero-list.component";
+import { HeroCreateComponent } from "./modules/features/heroes/pages/hero-create/hero-create.component";
+import { HeroEditComponent } from "./modules/features/heroes/pages/hero-edit/hero-edit.component";
 
 export const routes: Routes = [
 	{
 		path: "",
-		redirectTo: RouteEnum.HOME,
+		redirectTo: HeroSubRouteEnum.LIST,
 		pathMatch: "full",
 	},
 	{
-		path: RouteEnum.HOME,
-		component: PageHomeComponent,
+		path: HeroSubRouteEnum.LIST,
+		component: HeroListComponent,
 	},
 	{
-		path: RouteEnum.CREATE,
-		component: PageCreateComponent,
+		path: HeroSubRouteEnum.CREATE,
+		component: HeroCreateComponent,
 	},
 	{
-		path: RouteEnum.EDIT + "/:id",
-		component: PageEditComponent,
+		path: `${HeroSubRouteEnum.EDIT}/:id`,
+		component: HeroEditComponent,
 	},
-	{ path: "**", redirectTo: RouteEnum.HOME },
+	{ path: "**", redirectTo: HeroSubRouteEnum.LIST },
 ];
